4dcaf38c30ac6944b8a7c288dab144319c2ebce1
Alec <alec@Alec-Laptop>
2016-12-10 15:19:55 +0200

    Data from database

--- GreenICT/DatabaseHandler.cs
+++ GreenICT/DatabaseHandler.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace GreenICT
 {
@@ -12,6 +13,13 @@ namespace GreenICT
     {
         public DatabaseHandler()
         {
+
+        }
+
+        public String getImageUrl(int objectId)
+        {
+            String result = "Something went wrong";
+            int category = 1;
             string connStr = "server=localhost;user=root;database=green_ict;port=3306;password=;";
             MySqlConnection conn = new MySqlConnection(connStr);
             try
@@ -19,6 +27,12 @@ namespace GreenICT
                 Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
                 // Perform database operations
+                MySqlCommand cmd = new MySqlCommand("SELECT value FROM metadata WHERE Category_categoryid = " + category + " AND GameObject_GameObjectId = " + objectId, conn);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adp.Fill(ds, "LoadDataBinding");
+                Object raw = cmd.ExecuteScalar();
+                result = (String)raw;
             }
             catch (Exception ex)
             {
@@ -26,6 +40,7 @@ namespace GreenICT
             }
             conn.Close();
             Console.WriteLine("Done.");
+            return result;
         }
     }
 }
