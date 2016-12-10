9c32f45f93a2e7c25c240fbbc9f6aed0ca622820
Alec <alec@Alec-Laptop>
2016-12-10 14:49:41 +0200

    Database mysql connection

--- GreenICT/DatabaseHandler.cs
+++ GreenICT/DatabaseHandler.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace GreenICT
 {
     class DatabaseHandler
     {
+        public DatabaseHandler()
+        {
+            string connStr = "server=localhost;user=root;database=green_ict;port=3306;password=;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                // Perform database operations
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
     }
 }
